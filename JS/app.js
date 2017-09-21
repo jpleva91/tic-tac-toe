16aecdb66dcfba061b2bbd3b4b9fdc4ad1c6f537
jpleva91 <jpleva91@gmail.com>
2017-09-20 22:28:51 -0600

    adds clear board and h1 method

--- JS/app.js
+++ JS/app.js
@@ -4,11 +4,6 @@ console.log("JS is loaded!");
 /*	JQUERY LOADED  */
 $(document).ready(function() {
 	console.log("Ready to go!");
-	/*  GAME RESET  */
-	$('#clear-btn').on('click', function(evt) {
-		console.log(evt);
-		console.log(this);
-	});
 
 	/*  GAME FUNCTIONS */
 	var xMarker = false;
@@ -16,11 +11,23 @@ $(document).ready(function() {
 	var marker;
 	var lastGameState = 0;
 
+	/*  GAME RESET  */
+	function $gameReset() {
+		$('td').removeClass('x-move');
+		$('td').removeClass('o-move');
+		lastGameState = 0;
+	}
+
+	$('#clear-btn').on('click', function() {
+		$gameReset();
+	});
+
 	/* WINNING COMBINATIONS */
 	
 	// (1,2,3) (4,5,6) (7,8,9)
-	// (1,5,6) (1,4,7) (2,5,8)
+	// (1,5,9) (1,4,7) (2,5,8)
 	// (3,5,7) (3,6,9) 
+
 	
 
 	function $checkWinner() {
@@ -49,14 +56,14 @@ $(document).ready(function() {
 				( $('#box-7').hasClass('o-move') &&
 					$('#box-8').hasClass('o-move') &&
 					$('#box-9').hasClass('o-move') ) ||
-		// x_(1,5,6)
+		// x_(1,5,9)
 				( $('#box-1').hasClass('x-move') &&
 					$('#box-5').hasClass('x-move') &&
-					$('#box-6').hasClass('x-move') ) ||
-		// o_(1,5,6)
+					$('#box-9').hasClass('x-move') ) ||
+		// o_(1,5,9)
 				( $('#box-1').hasClass('o-move') &&
 				  $('#box-5').hasClass('o-move') &&
-				  $('#box-6').hasClass('o-move') ) ||
+				  $('#box-9').hasClass('o-move') ) ||
 		// x_(1,4,7)
 				( $('#box-1').hasClass('x-move') &&
 					$('#box-4').hasClass('x-move') &&
@@ -91,6 +98,7 @@ $(document).ready(function() {
 					$('#box-9').hasClass('o-move') )
 			) {
 			alert("winner!");
+			$gameReset();
 		}
 	}
 
@@ -106,22 +114,17 @@ $(document).ready(function() {
 	function endGame() {
 		if(lastGameState > 8){
 			alert("Game Over");
+			$gameReset();
 		}
 	}
-
-	function gameStateEven() {
-		if(lastGameState % 2 === 0) {
-			return true;
-		} else {
-			return false;
-		}
-	};
 
 	function placeMarker() {
 		if(lastGameState % 2 === 0){
 			marker = xMarker;
+			$('h1').text("It is X's turn");
 		} else {
 			marker = oMarker;
+			$('h1').text("It is O's turn");
 		}
 		console.log(marker);
 	};
@@ -176,33 +179,99 @@ $(document).ready(function() {
 		});
 
 		var clickBoxFour =
-		$('#box-4').on('click', function(evt) {
-		console.log(evt);
-		console.log(this);
+		$('#box-4').on('click', function() {
+			updateGameState();
+			endGame();
+			placeMarker();
+			if($('#box-4').hasClass('x-move') || $('#box-4').hasClass('o-move')) {
+				lastGameState = lastGameState -1;
+				return;
+			} if (!marker) {
+					$('#box-4').toggleClass('o-move').removeClass('x-move');
+			} else {
+					$('#box-4').toggleClass('x-move').removeClass('o-move');
+			}
+			$checkWinner();
 		})	
+
 		var clickBoxFive = 
-		$('#box-5').on('click', function(evt) {
-		console.log(evt);
-		console.log(this);
+		$('#box-5').on('click', function() {
+			updateGameState();
+			endGame();
+			placeMarker();
+			if($('#box-5').hasClass('x-move') || $('#box-5').hasClass('o-move')) {
+				lastGameState = lastGameState -1;
+				return;
+			} if (!marker) {
+					$('#box-5').toggleClass('o-move').removeClass('x-move');
+			} else {
+					$('#box-5').toggleClass('x-move').removeClass('o-move');
+			}
+			$checkWinner();
 		})	
+
 		var clickBoxSix = 
-		$('#box-6').on('click', function(evt) {
-		console.log(evt);
-		console.log(this);
+		$('#box-6').on('click', function() {
+			updateGameState();
+			endGame();
+			placeMarker();
+			if($('#box-6').hasClass('x-move') || $('#box-6').hasClass('o-move')) {
+				lastGameState = lastGameState -1;
+				return;
+			} if (!marker) {
+					$('#box-6').toggleClass('o-move').removeClass('x-move');
+			} else {
+					$('#box-6').toggleClass('x-move').removeClass('o-move');
+			}
+			$checkWinner();
 		})	
+
 		var clickBoxSeven = 
-		$('#box-7').on('click', function(evt) {
-		console.log(evt);
-		console.log(this);
+		$('#box-7').on('click', function() {
+			updateGameState();
+			endGame();
+			placeMarker();
+			if($('#box-7').hasClass('x-move') || $('#box-7').hasClass('o-move')) {
+				lastGameState = lastGameState -1;
+				return;
+			} if (!marker) {
+					$('#box-7').toggleClass('o-move').removeClass('x-move');
+			} else {
+					$('#box-7').toggleClass('x-move').removeClass('o-move');
+			}
+			$checkWinner();
 		})	
+
 		var clickBoxEight =
-		$('#box-8').on('click', function(evt) {
-		console.log(evt);
-		console.log(this);
+		$('#box-8').on('click', function() {
+			updateGameState();
+			endGame();
+			placeMarker();
+			if($('#box-8').hasClass('x-move') || $('#box-8').hasClass('o-move')) {
+				lastGameState = lastGameState -1;
+				return;
+			} if (!marker) {
+					$('#box-8').toggleClass('o-move').removeClass('x-move');
+			} else {
+					$('#box-8').toggleClass('x-move').removeClass('o-move');
+			}
+			$checkWinner();
 		})	
+
 		var clickBoxNine = 
-		$('#box-9').on('click', function(evt) {
-		console.log(evt);
-		console.log(this);
+		$('#box-9').on('click', function() {
+			updateGameState();
+			endGame();
+			placeMarker();
+			if($('#box-9').hasClass('x-move') || $('#box-9').hasClass('o-move')) {
+				lastGameState = lastGameState -1;
+				return;
+			} if (!marker) {
+					$('#box-9').toggleClass('o-move').removeClass('x-move');
+			} else {
+					$('#box-9').toggleClass('x-move').removeClass('o-move');
+			}
+			$checkWinner();
 		})
+
 });
