34f4803e24958d8fee89a160b94e83d9caf8f3bf
jpleva91 <jpleva91@gmail.com>
2017-09-20 22:43:41 -0600

    because jquery

--- JS/app.js
+++ JS/app.js
@@ -6,16 +6,16 @@ $(document).ready(function() {
 	console.log("Ready to go!");
 
 	/*  GAME FUNCTIONS */
-	var xMarker = false;
-	var oMarker = true;
-	var marker;
-	var lastGameState = 0;
+	var $xMarker = false;
+	var $oMarker = true;
+	var $marker;
+	var $lastGameState = 0;
 
 	/*  GAME RESET  */
 	function $gameReset() {
 		$('td').removeClass('x-move');
 		$('td').removeClass('o-move');
-		lastGameState = 0;
+		$lastGameState = 0;
 	}
 
 	$('#clear-btn').on('click', function() {
@@ -102,43 +102,43 @@ $(document).ready(function() {
 		}
 	}
 
-	function updateGameState(){
-	if(lastGameState > 8) {
-		lastGameState = 0;
+	function $updateGameState(){
+	if($lastGameState > 8) {
+		$lastGameState = 0;
 		} else {
-			lastGameState = lastGameState + 1;
-			console.log(lastGameState);
+			$lastGameState = $lastGameState + 1;
+			console.log($lastGameState);
 		};
 	};
 
-	function endGame() {
-		if(lastGameState > 8){
+	function $endGame() {
+		if($lastGameState > 8){
 			alert("Game Over");
 			$gameReset();
 		}
 	}
 
-	function placeMarker() {
-		if(lastGameState % 2 === 0){
-			marker = xMarker;
+	function $placeMarker() {
+		if($lastGameState % 2 === 0){
+			$marker = $xMarker;
 			$('h1').text("It is X's turn");
 		} else {
-			marker = oMarker;
+			$marker = $oMarker;
 			$('h1').text("It is O's turn");
 		}
-		console.log(marker);
+		console.log($marker);
 	};
 
 	/*	GAME CONTROLLER  */
-		var clickBoxOne = 
+		var $clickBoxOne = 
 		$('#box-1').on('click', function() {
-			updateGameState();
-			endGame();
-		 	placeMarker();
+			$updateGameState();
+			$endGame();
+		 	$placeMarker();
 		 	if($('#box-1').hasClass('x-move') || $('#box-1').hasClass('o-move')){
-		 		lastGameState = lastGameState -1;
+		 		$lastGameState = $lastGameState -1;
 		 		return;
-		 	}	if(!marker){
+		 	}	if(!$marker){
 			 		$('#box-1').toggleClass('o-move').removeClass('x-move');
 			} else {
 					$('#box-1').toggleClass('x-move').removeClass('o-move');
@@ -146,15 +146,15 @@ $(document).ready(function() {
 			$checkWinner();
 		});
 
-		var clickBoxTwo = 
+		var $clickBoxTwo = 
 		$('#box-2').on('click', function() {
-			updateGameState();
-			endGame();
-			placeMarker();
+			$updateGameState();
+			$endGame();
+			$placeMarker();
 			if($('#box-2').hasClass('x-move') || $('#box-2').hasClass('o-move')) {
-				lastGameState = lastGameState -1;
+				$lastGameState = $lastGameState -1;
 				return;
-			} if (!marker) {
+			} if (!$marker) {
 					$('#box-2').toggleClass('o-move').removeClass('x-move');
 			}	else {
 					$('#box-2').toggleClass('x-move').removeClass('o-move');
@@ -162,15 +162,15 @@ $(document).ready(function() {
 			$checkWinner();
 		});
 
-		var clickBoxThree =
+		var $clickBoxThree =
 		$('#box-3').on('click', function() {
-			updateGameState();
-			endGame();
-			placeMarker();
+			$updateGameState();
+			$endGame();
+			$placeMarker();
 			if($('#box-3').hasClass('x-move') || $('#box-3').hasClass('o-move')) {
-				lastGameState = lastGameState -1;
+				$lastGameState = $lastGameState -1;
 				return;
-			}	if (!marker) {
+			}	if (!$marker) {
 					$('#box-3').toggleClass('o-move').removeClass('x-move');
 			}	else {
 					$('#box-3').toggleClass('x-move').removeClass('o-move');
@@ -178,15 +178,15 @@ $(document).ready(function() {
 			$checkWinner();
 		});
 
-		var clickBoxFour =
+		var $clickBoxFour =
 		$('#box-4').on('click', function() {
-			updateGameState();
-			endGame();
-			placeMarker();
+			$updateGameState();
+			$endGame();
+			$placeMarker();
 			if($('#box-4').hasClass('x-move') || $('#box-4').hasClass('o-move')) {
-				lastGameState = lastGameState -1;
+				$lastGameState = $lastGameState -1;
 				return;
-			} if (!marker) {
+			} if (!$marker) {
 					$('#box-4').toggleClass('o-move').removeClass('x-move');
 			} else {
 					$('#box-4').toggleClass('x-move').removeClass('o-move');
@@ -194,15 +194,15 @@ $(document).ready(function() {
 			$checkWinner();
 		})	
 
-		var clickBoxFive = 
+		var $clickBoxFive = 
 		$('#box-5').on('click', function() {
-			updateGameState();
-			endGame();
-			placeMarker();
+			$updateGameState();
+			$endGame();
+			$placeMarker();
 			if($('#box-5').hasClass('x-move') || $('#box-5').hasClass('o-move')) {
-				lastGameState = lastGameState -1;
+				$lastGameState = $lastGameState -1;
 				return;
-			} if (!marker) {
+			} if (!$marker) {
 					$('#box-5').toggleClass('o-move').removeClass('x-move');
 			} else {
 					$('#box-5').toggleClass('x-move').removeClass('o-move');
@@ -210,15 +210,15 @@ $(document).ready(function() {
 			$checkWinner();
 		})	
 
-		var clickBoxSix = 
+		var $clickBoxSix = 
 		$('#box-6').on('click', function() {
-			updateGameState();
-			endGame();
-			placeMarker();
+			$updateGameState();
+			$endGame();
+			$placeMarker();
 			if($('#box-6').hasClass('x-move') || $('#box-6').hasClass('o-move')) {
-				lastGameState = lastGameState -1;
+				$lastGameState = $lastGameState -1;
 				return;
-			} if (!marker) {
+			} if (!$marker) {
 					$('#box-6').toggleClass('o-move').removeClass('x-move');
 			} else {
 					$('#box-6').toggleClass('x-move').removeClass('o-move');
@@ -226,15 +226,15 @@ $(document).ready(function() {
 			$checkWinner();
 		})	
 
-		var clickBoxSeven = 
+		var $clickBoxSeven = 
 		$('#box-7').on('click', function() {
-			updateGameState();
-			endGame();
-			placeMarker();
+			$updateGameState();
+			$endGame();
+			$placeMarker();
 			if($('#box-7').hasClass('x-move') || $('#box-7').hasClass('o-move')) {
-				lastGameState = lastGameState -1;
+				$lastGameState = $lastGameState -1;
 				return;
-			} if (!marker) {
+			} if (!$marker) {
 					$('#box-7').toggleClass('o-move').removeClass('x-move');
 			} else {
 					$('#box-7').toggleClass('x-move').removeClass('o-move');
@@ -242,15 +242,15 @@ $(document).ready(function() {
 			$checkWinner();
 		})	
 
-		var clickBoxEight =
+		var $clickBoxEight =
 		$('#box-8').on('click', function() {
-			updateGameState();
-			endGame();
-			placeMarker();
+			$updateGameState();
+			$endGame();
+			$placeMarker();
 			if($('#box-8').hasClass('x-move') || $('#box-8').hasClass('o-move')) {
-				lastGameState = lastGameState -1;
+				$lastGameState = $lastGameState -1;
 				return;
-			} if (!marker) {
+			} if (!$marker) {
 					$('#box-8').toggleClass('o-move').removeClass('x-move');
 			} else {
 					$('#box-8').toggleClass('x-move').removeClass('o-move');
@@ -258,15 +258,15 @@ $(document).ready(function() {
 			$checkWinner();
 		})	
 
-		var clickBoxNine = 
+		var $clickBoxNine = 
 		$('#box-9').on('click', function() {
-			updateGameState();
-			endGame();
-			placeMarker();
+			$updateGameState();
+			$endGame();
+			$placeMarker();
 			if($('#box-9').hasClass('x-move') || $('#box-9').hasClass('o-move')) {
-				lastGameState = lastGameState -1;
+				$lastGameState = $lastGameState -1;
 				return;
-			} if (!marker) {
+			} if (!$marker) {
 					$('#box-9').toggleClass('o-move').removeClass('x-move');
 			} else {
 					$('#box-9').toggleClass('x-move').removeClass('o-move');
